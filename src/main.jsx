fae16a099968a2a8341a5db5788db683e11ee76e
YAO YU AN <bbb69543@gmail.com>
2025-06-22 15:53:04 +0800

    Add vercel analytics & speed-insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'; // 引入 SpeedInsights
+import { Analytics } from '@vercel/analytics/react'; // 引入 analytics
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
